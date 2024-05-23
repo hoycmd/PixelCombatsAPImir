08ca0c13badf6c334a0c54b63c18ab41a0055541
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-05-23 19:35:03 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -14,10 +14,42 @@ LeaderBoard.PlayerLeaderBoardValues = [
   new DisplayValueHeader("Smepti", "<b><i>Смерти</i></b>", "<b><i>Смерти</i></b>"),
   new DisplayValueHeader("Scores", "<b><i>Очки</i></b>", "<b><i>Очки</i></b>"),
 ];
-// Teams.RedBlueTeam = DesTeams.Getcontext;
+// Teams.RedBlueTeam = DesTeams.Getcontext
 Teams.Get("Blue").Properties.Get("Deaths").Value = "<b><i><color=Blue>Синие</a></i></b>";
 Teams.Get("Red").Properties.Get("Deaths").Value = "<b><i><color=Red>Красные</a></i></b>";
-//
+// Урон за очки
+var immortalityTimerName = "immortality";
+Spawns.GetContext().OnSpawn.Add(function(player){
+  player.Properties.Immortality.Value = true;
+  timer = player.Timers.Get(immortalityTimerName).Restart(5);
+});
+Timers.OnPlayerTimer.Add(function(timer){
+  if (timer.Id != immortalityTimerName) return;
+  timer.Player.Properties.Immortality.Value = false;
+});
+
+Damage.OnDeath.Add(function(player) {
+  ++player.Properties.Deaths.Value;
+});
+Damage.OnDamage.Add(function(player, damaged, damage) {
+  if (GameMode.Parameters.GetBool("scoresOnDamage")) {
+    if (player.id != damaged.id) player.Properties.Scores.Value += Math.ceil(damage);
+  }
+});
+Damage.OnKill.Add(function(player, killed) {
+  if (player.id !== killed.id) { 
+    ++player.Properties.Kills.Value;
+    player.Properties.Scores.Value += 250;
+  }
+});
+// Inventory = Inventory.Value = false;
+Player.Inventory.Main.("Blue").Value = false;
+Player.Inventory.Secondary.("Blue").Value = false;
+Player.Inventory.Explosive.("Blue").Value = false;
+Player.Inventory.Build.("Blue").Value = false;
+Player.Inventory.BuildInfinity.("Blue").Value = false;
+
+
 
 
 
